dbe533c825348052b206063d1ae8138cff687d36
qy-chang19 <aidanchangqingyu@gmail.com>
2025-08-05 15:16:05 +0000

    finished with chapter 1

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,8 +4,16 @@
 
 #include <iostream>
 
+int a, b = 5;
+
 int main(){
-    
+    bool my_flag;
+    a = 7;
+    my_flag = false; 
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "flag = " << my_flag << std::endl;
+
     std::cout << std::endl << std::endl;
     return 0;
 }
